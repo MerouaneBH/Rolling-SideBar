e53c3c749be22f3b19e42ecb603263152c0a250e
merouane <merouane@gmail.com>
2024-10-07 11:24:34 +0100

    feat-backgound added

---
+++ script.js
@@ -0,0 +1,8 @@
+const background = document.querySelector('.center-content')
+
+background.addEventListener('click',(e)=>{ 
+
+    document.querySelector('body').classList.toggle('second-color')
+
+})
+
